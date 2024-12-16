2b44ed5d6ebbb62893fc348fb63c57ae92f4218f
Jordy Rolando Tejada Lazo <jtejadala@unsa.edu.pe>
2024-12-16 16:29:09 -0500

    Pruebas y Debuggin - Test cargar escena y documentacion de todos los scripts con comentarios

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    // Método público que permite cargar una escena por su nombre
     public void loadScene(string sceneName)
     {
+        // Carga la escena especificada por el nombre
         SceneManager.LoadScene(sceneName);
+
+        // Muestra un mensaje en la consola para indicar que se ha llamado a la función
         Debug.Log("Gaaa");
     }
 }
